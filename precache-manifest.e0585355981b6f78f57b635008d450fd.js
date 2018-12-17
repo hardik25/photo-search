8cb55fa7976de5379b14eea3a804ed29d47db584
Hardik Tewari <htewari@usc.edu>
2018-12-16 21:27:54 -0800

    First Commit

---
+++ precache-manifest.e0585355981b6f78f57b635008d450fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "851c2ea28c98cc395650",
+    "url": "/photo-search/static/js/runtime~main.851c2ea2.js"
+  },
+  {
+    "revision": "20ca611d33f74818a8fe",
+    "url": "/photo-search/static/js/main.20ca611d.chunk.js"
+  },
+  {
+    "revision": "dda695a3091ade7f9d9f",
+    "url": "/photo-search/static/js/1.dda695a3.chunk.js"
+  },
+  {
+    "revision": "20ca611d33f74818a8fe",
+    "url": "/photo-search/static/css/main.523026ee.chunk.css"
+  },
+  {
+    "revision": "c62c7e791e573c9e1006e9440feabc45",
+    "url": "/photo-search/index.html"
+  }
+];
